54c02a9ed36f3c1f3461e9f0f201fa5cd86c3095
nNargesmohammadi <NargesMohammadii@protonmail.com>
2022-07-22 17:24:39 +0430

    prettytables class and its methods

--- Day16-1.py
+++ Day16-1.py
@@ -1,18 +1,29 @@
-import another_modoule
+# import another_module
 
-print(another_modoule.another_variable)
+# print(another_module.another_variable)
 
 # import turtle
 
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
 
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color('coral')
-timmy.forward(100)
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color('coral')
+# timmy.forward(100)
 
-my_screen = Screen()
-print(my_screen.canvwidth)
+# my_screen = Screen()
+# print(my_screen.bandwidth)
 
-my_screen.exitonclick()
+# my_screen.exitonclick()
+
+
+# import prettytable
+
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "water", "Fire"])
+table.align = "l"
+
+print(table)
