6590d4afdf2a1a86f7c5691a7f6859c70776f80a
nNargesmohammadi <NargesMohammadii@protonmail.com>
2022-07-23 21:03:10 +0430

    create check answer method

--- maiin.py
+++ maiin.py
@@ -12,3 +12,6 @@ for question in question_data:
 quiz = QuizBrain(question_bank)
 while quiz.still_has_question():
     quiz.next_question()
+
+print("you've completed the quiz.")
+print(f"you're final score was {quiz.score}/{len(question_bank)}")
