92f2b2f518b470e03d36b15dc444fa73abd1e415
nNargesmohammadi <NargesMohammadii@protonmail.com>
2022-07-23 18:02:41 +0430

    create a class, an object and an attribute

---
+++ Day17-1.py
@@ -0,0 +1,19 @@
+# create a class
+class User:
+    pass
+
+
+# create an object
+user_one = User()
+
+# create an attribute
+user_one.id = "001"
+user_one.username = "Mitra"
+
+print(user_one.username)
+print(user_one.id)
+
+user_two = User()
+user_two.id = "002"
+user_two.username = "Mina"
+
