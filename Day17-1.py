3efa45dc5b87ac15406e813d9ba868f57fa4f599
nNargesmohammadi <NargesMohammadii@protonmail.com>
2022-07-23 18:46:16 +0430

    create a class, object and  attribute

--- Day17-1.py
+++ Day17-1.py
@@ -1,19 +1,27 @@
 # create a class
 class User:
-    pass
+    # pass
+    def __init__(self, user_id, username):
+        print("new user being created.")
+        self.id = user_id
+        self.username = username
+        self.followers = 0
 
 
 # create an object
-user_one = User()
+user_one = User("100", "Mitra")
 
 # create an attribute
-user_one.id = "001"
-user_one.username = "Mitra"
+# user_one.id = "001"
+# user_one.username = "Mitra"
 
-print(user_one.username)
+# print(user_one.username)
 print(user_one.id)
 
-user_two = User()
-user_two.id = "002"
-user_two.username = "Mina"
+# user_two = User()
+# user_two.id = "002"
+# user_two.username = "Mina"
+user_two = User("002", "Mina")
+print(user_two.username)
+print(user_one.followers)
 
