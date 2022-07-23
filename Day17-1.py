9acecd037568731b687db6ffb2b9c5b3d4389fc1
nNargesmohammadi <NargesMohammadii@protonmail.com>
2022-07-23 19:02:38 +0430

    create a class, object and  attribute

--- Day17-1.py
+++ Day17-1.py
@@ -6,6 +6,12 @@ class User:
         self.id = user_id
         self.username = username
         self.followers = 0
+        self.following = 0
+
+    # adding methods to a class
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
 
 # create an object
@@ -22,5 +28,16 @@ print(user_one.id)
 # user_two.id = "002"
 # user_two.username = "Mina"
 user_two = User("002", "Mina")
-print(user_two.username)
+# print(user_two.username)
+# print(user_one.followers)
+user_one.follow(user_two)
 print(user_one.followers)
+print(user_one.following)
+print(user_two.followers)
+print(user_two.following)
+
+user_two.follow(user_one)
+print(user_one.followers)
+print(user_one.following)
+print(user_two.followers)
+print(user_two.following)
