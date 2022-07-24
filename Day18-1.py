63991528e7eed670b9219ebdf2aed530f06e4f9d
nNargesmohammadi <NargesMohammadii@protonmail.com>
2022-07-24 12:47:04 +0430

    Turtle class draw a square

--- Day18-1.py
+++ Day18-1.py
@@ -3,14 +3,26 @@ from turtle import Turtle, Screen
 timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("red")
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(150)
-timmy_the_turtle.left(80)
-timmy_the_turtle.forward(50)
-timmy_the_turtle.left(150)
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.right(90)
+# timmy_the_turtle.forward(150)
+# timmy_the_turtle.left(80)
+# timmy_the_turtle.forward(50)
+# timmy_the_turtle.left(150)
+timmy_the_turtle.shape("triangle")
+timmy_the_turtle.color("green")
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.right(90)
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.right(90)
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.right(90)
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.right(90)
 
-
+for _ in range(4):
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(90)
 
 
 
