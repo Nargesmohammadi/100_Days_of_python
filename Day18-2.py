ed98607ab9014e07c4a5e9eb2091c7ca5033f497
nNargesmohammadi <NargesMohammadii@protonmail.com>
2022-07-24 13:16:35 +0430

    draw a dashed line

---
+++ Day18-2.py
@@ -0,0 +1,27 @@
+# for once or twice
+# import turtle
+
+# tim = turtle.Turtle()
+
+# for several time or more
+from turtle import Turtle
+
+# tim1 = Turtle
+
+
+# create a new object
+import turtle as t
+tim2 = t.Turtle()
+
+for _ in range(15):
+    tim2.forward(10)
+    tim2.penup()
+    tim2.forward(10)
+    tim2.pendown()
+
+
+
+
+# import heroes
+# print(heroes.gen())
+
