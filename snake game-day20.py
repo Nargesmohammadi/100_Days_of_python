656e885821d07ab92649bde344b310bd38a7f6b7
nNargesmohammadi <NargesMohammadii@protonmail.com>
2022-07-25 20:58:57 +0430

    the snake automatically to move forwards

--- snake game-day20.py
+++ snake game-day20.py
@@ -1,15 +1,44 @@
 from turtle import Screen, Turtle
-
+import time
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My snake game")
-
+screen.tracer(0)
 # create a snake body
-snake = Turtle()
-snake_x_positions = [0, -20, -40]
-for snake_index in range(0, 2):
+
+snake_positions = [(0, 0) , (-20, 0), (-40, 0)]
+
+segments = []
+
+# for snake_index in range(0, 3):
+for positions in snake_positions:
     snake = Turtle("square")
     snake.color("white")
-    snake.goto(x=snake_x_positions[snake_index], y=0)
+    snake.penup()
+    # snake.goto(x=snake_x_positions[snake_index], y=0)
+    snake.goto(positions)
+    segments.append(snake)
 
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(1)
+    # for seg in segments:
+        # seg.forward(20)
+    # for seg_num in range(start=2, stop=0, step=-1):
+    for seg_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(20)
+
+
+
+
+
+
+
+screen.exitonclick()
