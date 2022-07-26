4824dfe8996eace877d8de70baee5feac5169bb1
nNargesmohammadi <NargesMohammadii@protonmail.com>
2022-07-26 18:49:28 +0430

    make the snake class and use it.

--- snake game-day20.py
+++ snake game-day20.py
@@ -1,44 +1,44 @@
 from turtle import Screen, Turtle
+from snake import Snake
 import time
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My snake game")
 screen.tracer(0)
+snake = Snake()
 # create a snake body
 
-snake_positions = [(0, 0) , (-20, 0), (-40, 0)]
+# snake_positions = [(0, 0), (-20, 0), (-40, 0)]
 
-segments = []
+# segments = []
 
 # for snake_index in range(0, 3):
-for positions in snake_positions:
-    snake = Turtle("square")
-    snake.color("white")
-    snake.penup()
-    # snake.goto(x=snake_x_positions[snake_index], y=0)
-    snake.goto(positions)
-    segments.append(snake)
+# for positions in snake_positions:
+# snake = Turtle("square")
+# snake.color("white")
+# snake.penup()
+# snake.goto(x=snake_x_positions[snake_index], y=0)
+# snake.goto(positions)
+# segments.append(snake)
 
 
 game_is_on = True
 
 while game_is_on:
     screen.update()
-    time.sleep(1)
-    # for seg in segments:
-        # seg.forward(20)
-    # for seg_num in range(start=2, stop=0, step=-1):
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
+    time.sleep(0.1)
+    snake.move_snake()
 
-
-
-
-
+    # # for seg in segments:
+    # # seg.forward(20)
+# # for seg_num in range(start=2, stop=0, step=-1):
+# for seg_num in range(len(segments) - 1, 0, -1):
+# new_x = segments[seg_num - 1].xcor()
+# new_y = segments[seg_num - 1].ycor()
+# segments[seg_num].goto(new_x, new_y)
+# segments[0].forward(20)
 
 
 screen.exitonclick()
---
+++ snake.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+
+SNAKE_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+
+MOVE_DISTANCE = 20
+
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+
+    def create_snake(self):
+        for positions in SNAKE_POSITIONS:
+            snake = Turtle("square")
+            snake.color("white")
+            snake.penup()
+            snake.goto(positions)
+            self.segments.append(snake)
+
+    def move_snake(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].forward(MOVE_DISTANCE)
