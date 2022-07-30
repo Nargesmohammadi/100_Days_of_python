f058960249f16bd89b4f35f2906df1869148b63f
nNargesmohammadi <NargesMohammadii@protonmail.com>
2022-07-30 18:53:49 +0430

    create a scoreboard and keep score.

--- scoreboard.py
+++ scoreboard.py
@@ -1,2 +1,22 @@
-class :
-    def __init__(self, ):
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+    def __int__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        # self.write(arg="score", align="center", font=("Arial", 24, "normal"))
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(arg="score", align="center", font=("Arial", 24, "normal"))
+
+    def increase_score(self):
+        self.score += 1
+        # self.write(arg="score", align="center", font=("Arial", 24, "normal"))
+        self.clear()
+        self.update_scoreboard()
--- snake game-day20.py
+++ snake game-day20.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
 
@@ -12,7 +13,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
-
+scoreboard = Scoreboard()
 screen.listen()
 
 screen.onkey(snake.snake_up, "Up")
@@ -45,6 +46,9 @@ while game_is_on:
     # Detect collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
+        scoreboard.increase_score()
+
+
 
     # # for seg in segments:
     # # seg.forward(20)
